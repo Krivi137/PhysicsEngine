9c60213ea93a0868913de2ac3c0b35ed4fe30c0c
Krivi <koshinkhatri@gmail.com>
2026-07-03 19:22:49 +0530

    Create initial SFML window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    std::cout << "Hello Physics Engine!\n";
-
-    return 0;
+    sf::Window window;
+    window.create(sf::VideoMode({800, 600}), "My window");
+    
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        while (const std::optional event = window.pollEvent())
+        {
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+    }
 }
