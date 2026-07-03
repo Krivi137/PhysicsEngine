3bba581df26e48288f032d179b82568b6d7ed9f9
Krivi <koshinkhatri@gmail.com>
2026-07-03 20:09:26 +0530

    Switched to RenderWindow and change background color to black

--- src/Engine/Application.cpp
+++ src/Engine/Application.cpp
@@ -1,6 +1,6 @@
 #include "Engine/Application.hpp"
-
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 Application::Application()
 {
@@ -8,9 +8,9 @@ Application::Application()
 
 void Application::run()
 {
-    sf::Window window;
-    window.create(sf::VideoMode({800, 600}), "Physics Engine");
-    
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Physics Engine");
+    window.setFramerateLimit(60);
+
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
@@ -20,5 +20,10 @@ void Application::run()
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
+        
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        window.display();
     }
 }
